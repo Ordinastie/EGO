38788cd9eaf481d01fbc70426a649dd0af8d6106
Ordinastie <ordinastie@hotmail.com>
2019-04-11 18:44:20 +0200

    Removed configurable color

--- src/main/java/net/malisis/ego/gui/render/background/OptionPanel.java
+++ src/main/java/net/malisis/ego/gui/render/background/OptionPanel.java
@@ -17,33 +17,34 @@ public class OptionPanel implements IGuiRenderer
 	private GuiShape middle;
 	private GuiShape bottom;
 
-	public OptionPanel(UIComponent component, int shadeColor)
+	public OptionPanel(UIComponent component)
 	{
 		IntSupplier width = Sizes.widthRelativeTo(component, 1, 0);
-		top = GuiShape.builder(component).size(width, BORDER_HEIGHT).color(shadeColor).bottomAlpha(120).build();
+		top = GuiShape.builder(component)
+					  .size(width, BORDER_HEIGHT)
+					  .color(0x000000)
+					  .bottomAlpha(120)
+					  .build();
 		middle = GuiShape.builder(component)
 						 .position(s -> Position.below(s, top, 0))
 						 .size(width, Sizes.heightRelativeTo(component, 1, BORDER_HEIGHT * -2))
-						 .color(shadeColor)
+						 .color(0x000000)
 						 .alpha(120)
 						 .build();
 		bottom = GuiShape.builder(component)
 						 .position(s -> Position.below(s, middle, 0))
 						 .size(width, BORDER_HEIGHT)
-						 .color(shadeColor)
+						 .color(0x000000)
 						 .topAlpha(120)
 						 .build();
 
 	}
 
-	public OptionPanel(UIComponent component)
-	{
-		this(component, 0x000000);
-	}
-
 	@Override
 	public void render(GuiRenderer renderer)
 	{
-		top.and(middle).and(bottom).render(renderer);
+		top.and(middle)
+		   .and(bottom)
+		   .render(renderer);
 	}
 }
