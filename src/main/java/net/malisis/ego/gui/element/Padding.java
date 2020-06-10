271ed386c438e6d00c2b61cb813ff6826939bcbf
Ordinastie <ordinastie@hotmail.com>
2020-06-10 16:46:46 +0200

    Reverted back to interface Added static helper methods

--- src/main/java/net/malisis/ego/gui/element/Padding.java
+++ src/main/java/net/malisis/ego/gui/element/Padding.java
@@ -27,66 +27,44 @@ package net.malisis.ego.gui.element;
 /**
  * @author Ordinastie
  */
-public class Padding
+public interface Padding
 {
-	public static final Padding NO_PADDING = new Padding(0, 0, 0, 0);
-
-	protected final int top, bottom, left, right;
-
-	protected Padding(int top, int bottom, int left, int right)
-	{
-		this.top = top;
-		this.bottom = bottom;
-		this.left = left;
-		this.right = right;
-	}
+	Padding NO_PADDING = new FixedPadding(0, 0, 0, 0);
 
 	/**
 	 * Left padding.
 	 *
 	 * @return the left padding
 	 */
-	public int left()
-	{
-		return left;
-	}
+	int left();
 
 	/**
 	 * Right padding.
 	 *
 	 * @return the right padding
 	 */
-	public int right()
-	{
-		return right;
-	}
+	int right();
 
 	/**
 	 * Top padding.
 	 *
 	 * @return the top padding.
 	 */
-	public int top()
-	{
-		return top;
-	}
+	int top();
 
 	/**
 	 * Bottom padding.
 	 *
 	 * @return the bottom padding.
 	 */
-	public int bottom()
-	{
-		return bottom;
-	}
+	int bottom();
 
 	/**
 	 * Horizontal padding.
 	 *
 	 * @return the horizontal padding
 	 */
-	public int horizontal()
+	default int horizontal()
 	{
 		return left() + right();
 	}
@@ -96,30 +74,63 @@ public class Padding
 	 *
 	 * @return the vertical padding
 	 */
-	public int vertical()
+	default int vertical()
 	{
 		return top() + bottom();
 	}
 
-	@Override
-	public String toString()
+	class FixedPadding implements Padding
 	{
-		return "Pad{" + top() + "." + bottom() + "." + left() + "." + right() + "}";
+		protected final int top, bottom, left, right;
+
+		protected FixedPadding(int top, int bottom, int left, int right)
+		{
+			this.top = top;
+			this.bottom = bottom;
+			this.left = left;
+			this.right = right;
+		}
+
+		public int left()
+		{
+			return left;
+		}
+
+		public int right()
+		{
+			return right;
+		}
+
+		public int top()
+		{
+			return top;
+		}
+
+		public int bottom()
+		{
+			return bottom;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "Pad{" + top() + "." + bottom() + "." + left() + "." + right() + "}";
+		}
 	}
 
-	public static Padding of(int padding)
+	static Padding of(int padding)
 	{
 		return Padding.of(padding, padding, padding, padding);
 	}
 
-	public static Padding of(int horizontal, int vertical)
+	static Padding of(int horizontal, int vertical)
 	{
 		return Padding.of(vertical, vertical, horizontal, horizontal);
 	}
 
-	public static Padding of(int top, int bottom, int left, int right)
+	static Padding of(int top, int bottom, int left, int right)
 	{
-		return top == 0 && bottom == 0 && left == 0 && right == 0 ? NO_PADDING : new Padding(top, bottom, left, right);
+		return top == 0 && bottom == 0 && left == 0 && right == 0 ? NO_PADDING : new FixedPadding(top, bottom, left, right);
 	}
 
 	/**
@@ -129,15 +140,49 @@ public class Padding
 	 * @param component object to get the padding from
 	 * @return the padding
 	 */
-	public static Padding of(Object component)
+	static Padding of(Object component)
 	{
-		return component instanceof IPadded ? ((IPadded) component).padding() : Padding.NO_PADDING;
+		if (component instanceof Padding)
+			return (Padding) component;
+		else if (component instanceof IPadded)
+			return ((IPadded) component).padding();
+		return Padding.NO_PADDING;
+	}
+
+	static int topOf(Object obj)
+	{
+		return of(obj).top();
+	}
+
+	static int bottomOf(Object obj)
+	{
+		return of(obj).left();
+	}
+
+	static int leftOf(Object obj)
+	{
+		return of(obj).left();
+	}
+
+	static int rightOf(Object obj)
+	{
+		return of(obj).right();
+	}
+
+	static int verticalOf(Object obj)
+	{
+		return of(obj).vertical();
+	}
+
+	static int horizontalOf(Object obj)
+	{
+		return of(obj).horizontal();
 	}
 
 	/**
 	 * IPadded defines an object that provides a Padding element.
 	 */
-	public interface IPadded
+	interface IPadded
 	{
 		default Padding padding()
 		{
