3cdb887af376e2b454425ba80a1411889f310af8
Ordinastie <ordinastie@hotmail.com>
2020-06-16 02:52:31 +0200

    Fixed FloatingLayout positioning

--- src/main/java/net/malisis/ego/gui/component/layout/FloatingLayout.java
+++ src/main/java/net/malisis/ego/gui/component/layout/FloatingLayout.java
@@ -2,24 +2,33 @@ package net.malisis.ego.gui.component.layout;
 
 import net.malisis.ego.gui.component.UIComponent;
 import net.malisis.ego.gui.component.container.UIContainer;
+import net.malisis.ego.gui.element.Margin;
 import net.malisis.ego.gui.element.Padding;
 import net.malisis.ego.gui.element.position.Position;
 
 public class FloatingLayout implements ILayout
 {
 	private final UIContainer parent;
+	private UIComponent last;
 	private int currentX, currentY;
 	private int lineHeight;
+
 	private final int spacing;
 
 	public FloatingLayout(UIContainer parent, int space)
 	{
 		this.parent = parent;
 		this.spacing = space;
-		currentX = Padding.of(parent)
-						  .left();
+		reset();
+	}
+
+	private void reset()
+	{
+		currentX = 0;
 		currentY = Padding.of(parent)
 						  .top();
+		last = null;
+
 	}
 
 	private Position.IPosition current()
@@ -30,40 +39,56 @@ public class FloatingLayout implements ILayout
 	private void updateCurrent(UIComponent component)
 	{
 		currentX += component.size()
-							 .width() + spacing;
-		lineHeight = Math.max(lineHeight, component.size()
-												   .height());
+							 .width() + Margin.of(component)
+											  .left() + spacing;
+
 	}
 
 	private void nextLine()
 	{
-		currentX = Padding.of(parent)
-						  .left();
-		currentY += lineHeight + spacing;
+		currentX = 0;
+		currentY += Margin.bottomOf(last) + lineHeight + spacing;
 		lineHeight = 0;
+		last = null;
+	}
+
+	private int spaceBefore(UIComponent component)
+	{
+		return last == null ? Margin.left(component) : Margin.horizontal(last, component) + spacing;
+	}
+
+	private void place(UIComponent component, int space)
+	{
+		component.setPosition(Position.of(currentX + space, currentY));
+		currentX += space + component.width();
 	}
 
 	@Override
 	public void add(UIComponent component)
 	{
-		if (currentX + component.size()
-								.width() > parent.size()
-												 .width())
-		{
-			nextLine();
-		}
-		component.setPosition(current());
-		updateCurrent(component);
+		int before = spaceBefore(component);
 
+		if (currentX + before + component.width() > parent.innerSize()
+														  .width() + Margin.right(component))
+			nextLine();
+
+		place(component, before);
+		lineHeight = Math.max(lineHeight, component.size()
+												   .height());
+		last = component;
 	}
 
 	@Override
 	public void remove(UIComponent component)
 	{
+		reset();
+		parent.components()
+			  .forEach(this::add);
 	}
 
 	@Override
 	public void clear()
 	{
+		reset();
 	}
 }
